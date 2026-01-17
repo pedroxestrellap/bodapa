d64f9558f3d7ecc18005738e1962580347f1cd9f
pedroxestrellap <pedrox.estrellap@gmail.com>
2026-01-16 23:16:16 -0400

    change json to db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
+import netlify from "@astrojs/netlify";
 import react from '@astrojs/react'
 
 export default defineConfig({
+  output: "hybrid",
+  adapter: netlify(),
   integrations: [react()],
   vite: {
     server: {
