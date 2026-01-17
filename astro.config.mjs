fb644c2079dc5f709a987f82ec834e38271e4946
pedroxestrellap <pedrox.estrellap@gmail.com>
2026-01-17 00:30:13 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 export default defineConfig({
-  output: "hybrid",
-  adapter: netlify(),
+  output: "server",
+
+  assets: {
+    enabled: false, // 🔴 ESTA ES LA CLAVE REAL
+  },
+
+  adapter: netlify({
+    edge: false, // necesario para Supabase
+  }),
   integrations: [react()],
 });
