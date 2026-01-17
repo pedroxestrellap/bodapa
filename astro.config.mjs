8e658e244b485961a09a8831bbcab681e74e3baf
pedroxestrellap <pedrox.estrellap@gmail.com>
2026-01-16 23:37:21 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
-import react from '@astrojs/react'
+import react from "@astrojs/react";
 
 export default defineConfig({
   output: "hybrid",
   adapter: netlify(),
   integrations: [react()],
-  vite: {
-    server: {
-      allowedHosts: [
-        'peroratorical-twiggier-marc.ngrok-free.dev'
-      ]
-    }
-  }
-})
+});
